faf8bfdc3f21c9b116f14329c696e1c5be568e06
Sam Adams <sam.adams7785@gmail.com>
2022-10-05 08:05:18 -0700

    initial js

--- app.js
+++ app.js
@@ -3,9 +3,21 @@
 import './auth/user.js';
 
 /* Get DOM Elements */
-
+const catogorySelect = document.getElementById('catogory-select');
+const form = document.getElementById('compose');
+const addPostButton = document.getElementById('add');
+const errorDisplay = document.getElementById('error');
 /* State */
-
+let error = null;
 /* Events */
 
 /* Display Functions */
+function displayError() {
+    if (error) {
+        // eslint-disable-next-line no-console
+        console.log(error);
+        errorDisplay.textContent = error.message;
+    } else {
+        errorDisplay.textContent = '';
+    }
+}
